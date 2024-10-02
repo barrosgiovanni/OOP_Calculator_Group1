faa80ba1a7a7a2f08cf58966eea94a400e795ede
“2023317MaxDyson <maxgdyson@gmail.com>
2024-10-01 22:00:50 -0600

    Completed ToPostFix Method and had to add a new method called GetOperator.

--- OOP_Calculator_Group1/Program.cs
+++ OOP_Calculator_Group1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -18,11 +19,11 @@ namespace OOP_Calculator_Group1
         public bool isLeftAssociative { get; set; } = true; // we're not using rn because we're not doing exp and srt
 
         public abstract float Execute(float operand1 = 0, float operand2 = 0);
-        private static bool IsOperator(char op) // method overloading to accept both char and string
+        public static bool IsOperator(char op) // method overloading to accept both char and string
         {
             return false;
         }
-        private static bool IsOperator(string op) // method overloading to accept both char and string
+       public static bool IsOperator(string op) // method overloading to accept both char and string
         {
             return false;
         }
@@ -30,7 +31,7 @@ namespace OOP_Calculator_Group1
 
     class Addition : Operator
     {
-        Addition()
+      public Addition()
         {
             Symbol = '+';
             Precedence = 1;
@@ -42,7 +43,7 @@ namespace OOP_Calculator_Group1
     }
     class Subtraction : Operator
     {
-        Subtraction()
+      public Subtraction()
         {
             Symbol = '-';
             Precedence = 1;
@@ -54,7 +55,7 @@ namespace OOP_Calculator_Group1
     }
     class Multiplication : Operator
     {
-        Multiplication()
+      public Multiplication()
         {
             Symbol = '*';
             Precedence = 2;
@@ -66,7 +67,7 @@ namespace OOP_Calculator_Group1
     }
     class Division : Operator
     {
-        Division()
+      public Division()
         {
             Symbol = '/';
             Precedence = 2;
@@ -78,7 +79,7 @@ namespace OOP_Calculator_Group1
     }
     class LeftParenthesis : Operator
     {
-        LeftParenthesis()
+     public LeftParenthesis()
         {
             Symbol = '(';
             Precedence = 3;
@@ -97,25 +98,213 @@ namespace OOP_Calculator_Group1
         // methods
         public static List<string> Tokenize (string userInput) // ass: Patricia // this method receives user input (a string) and returns a List of strings. Contains the tokenize expression.
         {
+           
+            //Example:
+            /*
+                user input: 1+ 2 - (-3 *4.15)
+                tokenized expression: { "1", "+", "2", "-", "(", "-3", "*", "4.15", ")" }
+             */
+
+
             return new List<string>();
         }
-        public static Stack<string> ToPostFix (List<string> infixExp) // ass: Max Dyson // this method receives a tokenized list of strings and converts it to a Stack of strings.
+        public static Stack<string> ToPostFix(List<string> infixExp) // ass: Max Dyson // this method receives a tokenized list of strings and converts it to a Stack of strings.
         {
-             
-            Console.WriteLine("Made a new branch called ToPostFix");
-            
-            return new Stack<string>(); 
+
+            /*
+               Here we use the shunting yard algorithm to build a postfix expression:
+               example:
+                   infix     postfix
+                   1 + 2  -->   12+
+                   1+2*3  -->   123*+
+               Hint: We use the ExpressionProcessor.Outputlist and OperatorStack to build the correct postfixExp for each infix expression
+            */
+
+
+
+            OutputList = new List<string>();
+            OperatorStack = new Stack<Operator>();
+
+        
+       
+            foreach(string tokens in infixExp) {
+
+                // Code inside
+                
+                // Checks if the tokenized list of strings contains any numbers
+
+                if (float.TryParse(tokens, out float number))
+                {
+
+                    //  Adds only the numbers from the tokenized list of strings to the OutputList
+                  
+                    OutputList.Add(tokens);
+
+                }
+
+
+                // Checks if the tokenized list of strings has any operators
+
+
+                if (tokens.Contains("+") || tokens.Contains("-") || tokens.Contains("*") ||
+                    tokens.Contains("/") || tokens.Contains("(") || tokens.Contains(")"))
+                {
+
+                    Operator op = GetOperator(tokens);
+
+
+                    if (op != null)
+                    {
+
+                        // While there is an operator at the top of the stack (Peek()) with greater or equal precedence
+                        // pop it from the stack to the OutputList.
+
+                        while (OperatorStack.Count > 0 && OperatorStack.Peek().Precedence >= op.Precedence)
+                        {
+
+                            // ToString converts a char symbol into a string 
+
+                            OutputList.Add(OperatorStack.Pop().Symbol.ToString());
+
+
+
+                        }
+
+
+                        // Push the current operator onto the OperatorStack
+                       
+                        OperatorStack.Push(op);
+
+                    }
+
+
+
+                    // Push the left parenthesis onto the OperatorStack
+
+                    if (tokens == "(")
+                    {
+
+
+                        Operator lp = new LeftParenthesis();
+                        OperatorStack.Push(lp);
+
+                    }
+
+
+                 
+               
+
+
+                    if (tokens == ")")
+                    {
+
+
+                        // Pop the right parenthesis onto the OutputList until a left parenthesis is at the top of the OperatorStack ((Peek())).
+
+                        while (OperatorStack.Count > 0 && OperatorStack.Peek().Symbol.ToString() != "(") 
+                        {
+
+                          OutputList.Add(OperatorStack.Pop().Symbol.ToString());
+
+
+                        }
+
+
+                        // Remove the left parenthesis from the stack but do not add it to the OutputList
+
+                        if (OperatorStack.Count > 0 && OperatorStack.Pop().Symbol.ToString() == "(")
+                        {
+
+                            OperatorStack.Pop().Symbol.ToString();
+
+                           
+                        }
+
+                       
+                    }
+               
+             }
+
+               
+         }
+
+
+         
+        return new Stack<string>(OutputList); 
+    }
+      
+       
+        
+        // Return a Operator object
+        public static Operator GetOperator(string tokens)
+        {
+
+            switch (tokens){
+
+                case "+":
+
+                    return new Addition();
+
+                case "-":
+
+                    return new Subtraction();
+
+                case "*":
+
+                    return new Multiplication();
+
+
+                case "/":
+
+                    return new Division();
+
+                
+
+                default:
+
+                    return null;
+
+            }
+
+
         }
+
+
         public static string ExecutePostFix (Stack<string> postfixExp) // ass: Giovanni // this method receives a Stack of strings and return a postfix expression (string type).
         {
+
             return "";
         }
 
     }
     internal class Program
-    {        
+    {
+        //Encapsulation Coupling Method.
+        static void Calculate(string userInput)
+        {
+            List<string> tokenizedExp = ExpressionProcessor.Tokenize(userInput);
+            Stack<string> postfixExp = ExpressionProcessor.ToPostFix(tokenizedExp);
+            Console.WriteLine($"Result: {ExpressionProcessor.ExecutePostFix(postfixExp)}");
+        }
+
+
         static void Main(string[] args) // ass: Daniel 
         {
+            /*
+            As discussed in class... Feel free to play in here to test your methods.
+            Just remember to clear your changes before pushing, and to keep the methods execution chain as it is once you're done testing
+            please and thank you.
+            */
+            Console.WriteLine("-- Calculator --");
+            Console.WriteLine("Enter your expression:");
+            string mathExpression = "";
+            while (mathExpression.ToLower().Trim() != "exit")
+            {
+                mathExpression = Console.ReadLine();
+                Calculate(mathExpression);
+            }
+
+
         }
     }
 }
